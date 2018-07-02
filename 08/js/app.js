824ddd3b5a896a72d0859dadae5e29fe4f689f3d
Eugene Nakoneschniy <eugene.nakoneschniy@it-devgroup.com>
2018-07-02 23:52:38 +0300

    lesson 08 part 2

--- 08/js/app.js
+++ 08/js/app.js
@@ -21,7 +21,7 @@ do {
 } while (isNaN(x));
 console.log(x);
 
-for(let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
 
@@ -41,4 +41,60 @@ for (let i = 0; i <= 10; i++) {
         continue;
     }
     console.log(i);
+}
+console.log('-----------------');
+
+let arNumbers = [1, 2, 3, 4, 5, 6];
+let arStrings = ['fff', 'cccc', 'ddddd'];
+let arAnyData = ['fff', 3, true, 4, 'ssds'];
+console.log(arNumbers);
+console.log(arStrings);
+console.log(arAnyData);
+
+let temArray = [];
+temArray.push(3);
+temArray.push(4);
+temArray.push(35, 5, true, 'asdas');
+temArray.unshift(333);
+temArray.pop();
+
+console.log('==================');
+
+let arNumbers1 = [1, 2, true, 4, false, 6];
+
+for (let index = 0; index < arNumbers1.length; index++) {
+    console.log(index, arNumbers1[index]);
+}
+
+for (let index in arNumbers1) {
+    console.log(index, arNumbers1[index]);
+}
+
+for (let element of arNumbers1) {
+    console.log(element);
+}
+for (let element of arNumbers1) {
+    element = element + 3;
+}
+console.log(arNumbers1);
+for (let index in arNumbers1) {
+    arNumbers1[index] = arNumbers1[index] + 3;
+}
+console.log(arNumbers1);
+
+for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+        console.log(`${a} * ${b}:`, a * b);
+    }
+}
+
+let matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8]
+];
+console.log(matrix);
+for (let row = 0; row < matrix.length; row++) {
+    for (let cell = 0; cell < matrix[row].length; cell++) {
+        console.log(`matrix[${row}][${cell}]`, matrix[row][cell]);
+    }
 }
